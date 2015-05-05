7b5e969f1014a0bee1d8f28e0ff1b1f39009240a
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-05-05 12:08:24 +0300

    traceur working

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$traceurRuntime.options.symbols = true;
+System.registerModule("../src/main.js", [], function(require) {
+  "use strict";
+  var __moduleName = "../src/main.js";
+  var f = (function(x) {
+    return x * 92;
+  });
+  alert(f(52));
+  return {};
+});
+System.get("../src/main.js" + '');
---
+++ src/main.js
@@ -0,0 +1,3 @@
+const f = (x) => x * 92
+
+alert(f(52))
