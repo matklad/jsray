41f34b1f74d7fecafb815b7fdf375e291add98e8
pavel <sergeev.pavel.al@gmail.com>
2015-05-06 14:00:55 +0300

    phong reflection

--- src/scene.js
+++ src/scene.js
@@ -21,13 +21,21 @@ export const Scene = ({camera,
       return items.reduce(f, {t: -1, item:null})
     }
 
-    const _item_lighted_at = (item, pos) => {
+    const _item_lighted_at = (item, pos, viewer_dir) => {
       const f = (color, illuminator) => {
-        const dir = (illuminator.origin.sub(pos)).direction()
-        if (!_find_intersection(Ray(pos.add(dir.scale(1e-4)), dir)).item) {
-          const bright = item.normal_at(pos).dot(dir)
-          const light = illuminator.color.set_bright(bright)
-          return color.mix_with(item.color_at(pos).under_light(light))
+        const light_dir = (illuminator.origin.sub(pos)).direction()
+        if (!_find_intersection(Ray(pos.add(light_dir.scale(1e-4)), light_dir)).item) {
+          const normal = item.normal_at(pos)
+          const diffuse_bright = normal.dot(light_dir)
+          const diffuse_light = illuminator.color.set_bright(diffuse_bright)
+
+          const reflected_dir = normal.scale(2 * light_dir.dot(normal)).sub(light_dir)
+          const reflected_bright = Math.pow(reflected_dir.dot(viewer_dir), 2)
+          const reflected_light = illuminator.color.set_bright(reflected_bright)
+
+          const item_color = item.color_at(pos)
+          const with_diffuse = color.mix_with(item_color.under_light(diffuse_light))
+          return with_diffuse.mix_with(item_color.under_light(reflected_light))
         }
         return color
       }
@@ -45,7 +53,7 @@ export const Scene = ({camera,
 
       const ray = camera.cast_ray(dx, dy)
       const {t, item} = _find_intersection(ray)
-      return item ? _item_lighted_at(item, ray.point_along(t)) : background_color
+      return item ? _item_lighted_at(item, ray.point_along(t), ray.direction.scale(-1)) : background_color
     }
 
   }
